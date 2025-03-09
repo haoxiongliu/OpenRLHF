d8fda09c5e6d7ebf0a3da232bbe58bbd8f8a8ba9
liuhx20 <liuhx20@mails.tsinghua.edu.cn>
2025-03-10 01:27:25 +0800

    add prover

--- reward_fn/shorter.py
+++ reward_fn/shorter.py
@@ -1,7 +1,14 @@
 import torch
 from torch import Tensor
 
-def reward_func(queries, prompts):
+def reward_func(queries, prompts, labels):
     # queries is prompts + responses
     nega_query_lens = [-len(q)/100.0 for q in queries]
     return Tensor(nega_query_lens)
+
+if __name__ == "__main__":
+    queries = ["I am a prompt\n\n" + "I am a response"]
+    prompts = ["I am a prompt"]
+    labels = ["I am an answer"]
+
+    print(reward_func(queries, prompts, labels))
