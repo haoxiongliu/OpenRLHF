0261fd940bd125e9a0b7679b7b0dc16f08683fc4
liuhx20 <liuhx20@mails.tsinghua.edu.cn>
2025-03-14 08:59:26 +0800

    change default input template

--- openrlhf/cli/train_sft.py
+++ openrlhf/cli/train_sft.py
@@ -211,7 +211,7 @@ if __name__ == "__main__":
 
     parser.add_argument("--input_key", type=str, default="input", help="JSON dataset key")
     parser.add_argument("--output_key", type=str, default=None, help="JSON dataset key")
-    parser.add_argument("--input_template", type=str, default="User: {}\nAssistant: ")
+    parser.add_argument("--input_template", type=str, default=None)
     parser.add_argument(
         "--apply_chat_template", action="store_true", default=False, help="Use HF tokenizer chat template"
     )
