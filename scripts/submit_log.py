30dd0e226e09e72a53b52490d078118d74ddc6e2
foobar <foobar@qq.com>
2025-04-07 17:26:38 +0800

    no credential

--- scripts/submit_log.py
+++ scripts/submit_log.py
@@ -1,10 +1,18 @@
 from huggingface_hub import HfApi
 # upload a file to the hub
-api = HfApi()
-api.upload_file(
-    path_or_fileobj="logs/summary.log",
-    repo_id="Vivacem/results",
-    path_in_repo="summary.log",
-    token="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",
-    repo_type="dataset",
-)
+def submit_log(token):
+    api = HfApi()
+    api.upload_file(
+        path_or_fileobj="logs/summary.log",
+        repo_id="Vivacem/results",
+        path_in_repo="summary.log",
+        token=token,
+        repo_type="dataset",
+    )
+
+if __name__ == "__main__":
+    import argparse
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--token", type=str, help="Hugging Face token")
+    args = parser.parse_args()
+    submit_log(args.token)
